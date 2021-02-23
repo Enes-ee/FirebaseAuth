84e4d5d234c6aeaafbb2e5b2f4f00056098bc7cc
Enes Erden <aokiji.ee@hotmail.com>
2021-02-23 21:58:35 +0300

    Başlangıç şablonu oluşturuldu.

---
+++ app/src/main/java/com/enesproje/ee_firebaseauthexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.enesproje.ee_firebaseauthexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
